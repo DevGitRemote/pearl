ee73d42a2b17f0499eb56160a7c793c8ce3bdec2
DEVRAJ <122165702+DevGitRemote@users.noreply.github.com>
2023-08-11 05:02:15 +0530

    added new code

--- helloworld/app.py
+++ helloworld/app.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "This is the route."
+    return name
+
+@app.route('/hello')
+def hello():
+    name = "Hello world!"
+    return name
+
+if __name__ == "__main__":
+    app.run()
